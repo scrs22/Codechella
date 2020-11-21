810f590090e0bca5be9f572312136ce3fdf27b56
shruti <scrs.shruti22@gmail.com>
2020-11-21 17:53:11 -0500

    Add backbone code for all classes

---
+++ GoalDatabase.py
@@ -0,0 +1,4 @@
+Goals={"LoseWeight":["#Dieting","#MentalHealth","#Fitness","#Exercise"],
+       "QuitDrinking":["#rehab","#addiction","#alchoholism","#sobreity"],
+       "GetEmployed":["#HireMe","#jobsearch","#Unemployed","#hiring"]
+       }
