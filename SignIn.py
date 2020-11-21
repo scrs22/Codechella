bf015a1224fdab3430e744d63c1b57f9d477dd99
shruti <scrs.shruti22@gmail.com>
2020-11-21 13:32:55 -0500

    Add backbone code for all classes

--- SignIn.py
+++ SignIn.py
@@ -1,14 +1,20 @@
-#import some database
+from DatabaseDemo import data
 class SignIn:
     def __init__(self,username,password):
         self.username=username
         self.password=password
+        #@todo encrypt password
 
     def check_user(self):
-        
+        if self.username in data:
+            if data[self.username]["password"]==self.password:
+                return True
+        return False
 
-    def getInfo(self):
-        pass
 
-    def open_account(self):
-        pass
+    def getAccountAccess(self):
+        if self.check_user():
+            #display page for trends
+            #@Todo shreeram
+            pass
+
--- SignUp.py
+++ SignUp.py
@@ -1,13 +1,33 @@
-from DatabaseDemo
+from DatabaseDemo import data
+
 class SignUp:
     def __init__(self,username,password,twitterHandle,contact):
         self.username=username
         self.password=password
         self.twitterHandle=twitterHandle
         self.contact=contact
+        self.accesstoken=None
+        self.secretkey=None
+
+    def twitterSignIn(self):
+        pass
+
+    def checkUserNameExists(self):
+        if self.username in data:
+            return True
+        else:
+            return False
+
+    def addUser(self):
+        if checkUserNameExists():
+            return
+            #@todo ask for a different username
+        else:
+            data[s]
 
 
-    def checkUserExists(self):
+    def IncorrectUserName(self):
+        if
 
 
     def storeInfo(self):
